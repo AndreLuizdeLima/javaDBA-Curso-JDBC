f56f9890da7b27d6d7b40ec166b41cf47aad3c9f
Andre <andreluizjp2015@gmail.com>
2025-04-21 14:21:29 -0300

    21042025 14-21

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,10 +2,11 @@ import dto.DB;
 import exception.DbException;
 
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 
 public class Main {
     public static void main(String[] args) {
@@ -13,7 +14,9 @@ public class Main {
         //Connection conn = DB.getConnetion();
         //DB.closeConnection();
 
-        selectExemple();
+        //selectExemple();
+
+        insertExemple();
     }
 
     public static void selectExemple(){
@@ -42,4 +45,50 @@ public class Main {
 
 
     }
+
+    public static void insertExemple(){
+
+        Connection conn = null;
+        PreparedStatement st = null;
+
+        //LocalDateTime timeInsert = LocalDateTime.parse("2024-07-20T01:03:26");
+        LocalDate dateInsert = LocalDate.now();
+
+        try {
+            conn = DB.getConnetion();
+
+            st = conn.prepareStatement(
+                    "INSERT INTO seller (Name,Email,BirthDate,BaseSalary, DepartmentId) VALUES (?,?,?,?,?)"
+            , Statement.RETURN_GENERATED_KEYS);
+
+            st.setString(1,"Carl Purple");
+            st.setString(2,"carl@gmail.com");
+            //st.setTimestamp(3, Timestamp.valueOf(timeInsert));  //tempo completo com data e minuto
+            st.setDate(3,java.sql.Date.valueOf(dateInsert)); // tempo apenas data
+            st.setDouble(4, 3000.0);
+            st.setInt(5, 4);
+
+            int rowsAffected  = st.executeUpdate();
+
+            if (rowsAffected > 0){
+                ResultSet rs = st.getGeneratedKeys();
+                while(rs.next()){
+                    int id = rs.getInt(1);
+                    System.out.println("Done! id= " + id);
+                }
+            } else {
+                System.out.println("No rown affected!");
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            DB.closeStatement(st);
+            DB.closeConnection();
+        }
+
+
+    }
+
+
 }
