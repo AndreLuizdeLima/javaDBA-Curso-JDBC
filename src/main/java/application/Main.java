3a4d6cf6cb75f3899d606ad1d9e93af493be5665
Andre <andreluizjp2015@gmail.com>
2025-04-22 22:00:31 -0300

    22042025 22-00

--- src/main/java/application/Main.java
+++ src/main/java/application/Main.java
@@ -1,16 +1,19 @@
 package application;
 
 import db.Db;
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
+import model.entities.Seller;
 
 import java.sql.*;
 
 public class Main {
     public static void main(String[] args) {
 
-        Connection conn = Db.getConnetion();
-        Db.closeConnection();
+        SellerDao sellerdao = DaoFactory.createSellerDao();
+        Seller seller = sellerdao.findById(3);
 
-
+        System.out.println(seller);
     }
 }
